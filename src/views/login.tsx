5ca925ef0ae5d19d3f692aa06a86d17824ad6f1d
woyehaizaixiang <519089437@qq.com>
2020-05-29 16:52:59 +0800

    引入antd

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
-export default class login extends Component {
+import { Button } from 'antd';
+
+import styles from './login.module.scss';
+
+type Props = RouteComponentProps<any> & {
+    name: string;
+    enthusiasmLevel?: number;
+}
+type State = {
+    pageName?: string
+}
+
+export default class login extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+
+        }
+    }
     render() {
         return (
-            <div className="login">
-                login
+            <div className={styles.login}>
+                <Button type="primary" onClick={this.navigateToAdmin.bind(this, '/aaaa')}>登录</Button>
             </div>
         )
+    }
+    componentWillMount() {
+
+    }
+
+    navigateToAdmin(url: string) {
+        this.props.history.push(url);
     }
 }
