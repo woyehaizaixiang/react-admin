adfc7d23106f4644865c60b155e6cbea54d5c14f
woyehaizaixiang <519089437@qq.com>
2020-05-29 17:19:42 +0800

    登录页未完成

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
-import { Button } from 'antd';
+import { Form, Input, Button, Checkbox } from 'antd';
 
 import styles from './login.module.scss';
 
@@ -10,8 +10,16 @@ type Props = RouteComponentProps<any> & {
     enthusiasmLevel?: number;
 }
 type State = {
-    pageName?: string
+    pageName?: string;
 }
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
 
 export default class login extends Component<Props, State> {
     constructor(props: Props) {
@@ -23,15 +31,48 @@ export default class login extends Component<Props, State> {
     render() {
         return (
             <div className={styles.login}>
-                <Button type="primary" onClick={this.navigateToAdmin.bind(this, '/aaaa')}>登录</Button>
+                <div className={styles.loginForm}>
+                    <Form
+                        {...layout}
+                        name="basic"
+                        initialValues={{ remember: true }}
+                        onFinish={this.onFinish}
+                        onFinishFailed={this.onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         )
     }
     componentWillMount() {
 
     }
-
-    navigateToAdmin(url: string) {
-        this.props.history.push(url);
+    onFinish(values: any){
+        console.log('Success:', values);
+    }
+    onFinishFailed(errorInfo: any){
+        console.log('Failed:', errorInfo);
     }
 }
